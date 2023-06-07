1437813861533b2afb1d4fca96c8ef69d3cad5f6
faryz.coder@gmail.com <faryz.coder@gmail.com>
2023-06-08 01:41:26 +0800

    update directory

--- app/src/main/java/com/example/tenang_capstone/ui/directory/DirectoryFragment.java
+++ app/src/main/java/com/example/tenang_capstone/ui/directory/DirectoryFragment.java
@@ -1,5 +1,7 @@
 package com.example.tenang_capstone.ui.directory;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -24,9 +27,24 @@ public class DirectoryFragment extends Fragment {
         binding = FragmentDirectoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        directoryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textSlideshow;
+//        directoryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.ViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://sites.google.com/view/psymalaysia/english/selangor-kl?authuser=0";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
