c05ecb63e60fd4173a7b339508dd7c5f1651f79d
faryz.coder@gmail.com <faryz.coder@gmail.com>
2023-06-07 20:09:30 +0800

    update fragment name

--- app/src/main/java/com/example/tenang_capstone/ui/diary/DiaryViewModel.java
+++ app/src/main/java/com/example/tenang_capstone/ui/diary/DiaryViewModel.java
@@ -10,7 +10,7 @@ public class DiaryViewModel extends ViewModel {
 
     public DiaryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is diary fragment");
     }
 
     public LiveData<String> getText() {
