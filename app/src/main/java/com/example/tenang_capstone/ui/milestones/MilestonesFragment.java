9a92fe459d37b5fa6c2276a13d54caa77189f6f8
faryz.coder@gmail.com <faryz.coder@gmail.com>
2023-06-08 23:31:11 +0800

    update milestone

--- app/src/main/java/com/example/tenang_capstone/ui/milestones/MilestonesFragment.java
+++ app/src/main/java/com/example/tenang_capstone/ui/milestones/MilestonesFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -15,7 +17,7 @@ import com.example.tenang_capstone.databinding.FragmentMilestonesBinding;
 public class MilestonesFragment extends Fragment {
 
     private FragmentMilestonesBinding binding;
-
+    private int currentProgress = 0;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MilestonesViewModel milestonesViewModel =
@@ -24,9 +26,39 @@ public class MilestonesFragment extends Fragment {
         binding = FragmentMilestonesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        milestonesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textSlideshow;
+//        milestonesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ProgressBar milestoneProgress = binding.progressBar2;
+
+        milestoneProgress.setMax(90);
+
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int ml = 85;
+                if (ml <= 2) {
+                    currentProgress = ml *5;
+                } else if (ml <= 15) {
+                    currentProgress = (int) (ml * 3.7);
+                } else if (ml <= 30) {
+                    currentProgress = (int) (ml * 2.3);
+                } else if (ml <= 60) {
+                    currentProgress = (int) (ml * 1.5);
+                } else if (ml <=89) {
+                    currentProgress = ml;
+                } else {
+                    currentProgress = 90;
+                }
+                milestoneProgress.setProgress(currentProgress);
+            }
+        });
+
     }
 
     @Override
