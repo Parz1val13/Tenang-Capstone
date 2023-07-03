236edf20f36aeff9d21f2e16ee6b5c91109f2c41
faryz.coder@gmail.com <faryz.coder@gmail.com>
2023-07-03 11:05:10 +0800

    add search

--- app/src/main/java/com/example/tenang_capstone/ui/friends/FriendsFragment.java
+++ app/src/main/java/com/example/tenang_capstone/ui/friends/FriendsFragment.java
@@ -2,12 +2,15 @@ package com.example.tenang_capstone.ui.friends;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,6 +20,7 @@ import com.example.tenang_capstone.databinding.FragmentFriendsBinding;
 import com.example.tenang_capstone.ui.leaderboards.FriendsList;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -24,6 +28,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FriendsFragment extends Fragment {
 
@@ -34,6 +39,8 @@ public class FriendsFragment extends Fragment {
     List<FriendsList> friendsLists = new ArrayList<>();
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    String search =  "";
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -47,9 +54,34 @@ public class FriendsFragment extends Fragment {
         friendsView = binding.friendsView;
         friendsView.setLayoutManager(new LinearLayoutManager(requireContext()));
         friendsView.setAdapter(new FriendsAdapter(requireContext(), friendsLists));
-        getFriends();
+        getFriends(search);
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.textField.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                search = s.toString();
+            }
+        });
+
+        binding.textField.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("FriendsFragment", "search: "+search);
+                getFriends(search);
+            }
+        });
     }
 
     @Override
@@ -58,7 +90,7 @@ public class FriendsFragment extends Fragment {
         binding = null;
     }
 
-    public void getFriends() {
+    public void getFriends(String search) {
         CollectionReference docRef = db.collection("users");
         String uid = requireActivity().getIntent().getStringExtra("uid");
 
@@ -69,7 +101,12 @@ public class FriendsFragment extends Fragment {
                 friendsLists.clear();
                 for (QueryDocumentSnapshot document : task.getResult()) {
                     if (!document.getId().equals(uid)) {
-                        friendsLists.add(new FriendsList(document.getId(), (String) document.get("name")));
+                        if (!search.isEmpty() && document.get("name").toString().contains(search)) {
+                            friendsLists.add(new FriendsList(document.getId(), (String) document.get("name")));
+                        }
+                        if (search.isEmpty()){
+                            friendsLists.add(new FriendsList(document.getId(), (String) document.get("name")));
+                        }
                     }
                 }
                 friendsView.getAdapter().notifyDataSetChanged();
